69adbc4fb0fae3ef0d6bba92af23f4266333f337
Trinity <felipetrindade15th@gmail.com>
2026-08-16 21:06:06 -0400

    Ajeitado erros de digitação nos créditos do jogo

--- Scenes/User Interface/Score/Scripts/NewHighscore.cs
+++ Scenes/User Interface/Score/Scripts/NewHighscore.cs
@@ -7,6 +7,6 @@ public partial class NewHighscore : Godot.RichTextLabel
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Text = $"NEW HIGHSCORE: {Score.Highscore}";
+		Text = $"NOVO HIGHSCORE: {Score.Highscore}";
 	}
 }
